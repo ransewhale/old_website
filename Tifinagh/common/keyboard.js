020fdb657034d02c1f6c7271c00b50156ce1228e
卵生ホエール <ransewhale@rwmac.local>
2017-11-25 08:10:12 +0900

    Add Tifinagh

---
+++ Tifinagh/common/keyboard.js
@@ -0,0 +1,56 @@
+$(function(){
+
+function removeWrongChars(str){
+	var arr = "abgdDefkhH?xqi3lmnNpurRGsSXtcTvwjzZW \r\n".split('');
+	var data = str.split('');
+	var ret = "";
+	for(i=0; i<data.length; i++){
+		if(jQuery.inArray(data[i],arr) > -1){
+			ret = ret + data[i];
+		}
+	}
+	return ret;
+}
+
+function latinToTifinagh(str){
+	var lat = "abgdDefkhH?xqi3lmnNpurRGsSXtcTvwjzZW \r\n".split('');
+	var tif = "ⴰⴱⴳⴷⴹⴻⴼⴽⵀⵃⵄⵅⵇⵉⵊⵍⵎⵏⵑⵒⵓⵔⵕⵖⵙⵚⵛⵜⵞⵟⵠⵡⵢⵣⵥⵯ".split('');
+	tif.push("&nbsp;");
+	tif.push("<br />");
+	tif.push("<br />");
+	var ret = "";
+	for(i=0; i<str.length; i++){
+		var j = jQuery.inArray(str[i],lat);
+		ret = ret + tif[j];
+	}
+	return ret;
+}
+
+/*
+function transfer(str,pos){
+	var data = str.split('');
+	var ret = "";
+	for(i=0; i<data.length(); i++){
+		switch(data[i]){
+
+		}
+	}
+}
+*/
+
+var timer_id = setInterval( function () {
+},50);
+
+$("#input").keyup(function(){
+	var val = $("#input").val();
+	var str = removeWrongChars(val);
+	$("#input").val(str);
+	$("#output").html(latinToTifinagh(str));
+
+});
+
+$("#input").submit(function(){
+	return false;
+});
+
+});
